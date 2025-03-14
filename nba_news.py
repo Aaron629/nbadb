3dba9aacc864726e356a14a16bd4b15cb78d2975
unknown <aaron@hoone.com.tw>
2025-03-15 00:55:47 +0800

    add nba_news.py

---
+++ nba_news.py
@@ -0,0 +1,42 @@
+import requests
+import sys,os
+import django
+django.setup()
+sys.path.append('C:/Users/許皓倫/Downloads/interview/nbadb/nbadb')
+os.environ["DJANGO_SETTINGS_MODULE"]="nbadb.settings"
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from nbadata.models import Nba_news 
+import time
+
+s = Service("chromedriver.exe")
+driver = webdriver.Chrome(service=s)
+driver.get("https://tw-nba.udn.com/nba/index")
+res = requests.get('https://tw-nba.udn.com/nba/index')
+soup = BeautifulSoup(res.text, "html.parser")
+titles = soup.find_all('div', class_='box_body') 
+# print(titles)
+title_items =[]
+memo_items =[]
+up_time = datetime.now()
+up_user="aaron"
+total = 0 
+target_count = 500
+while target_count > len(title_items):
+  driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+  # 等待頁面加載
+  time.sleep(3)
+  for i in titles:
+    for j in i.find_all('h3'):
+      title_items.append(j.text[4:])
+      Nba_news.objects.create(title=j.text[4:],ldate=up_time,luser=up_user,iobool=True)
+      for k in i.find_all('p'):
+        memo_items.append(k.text)
+        Nba_news.objects.update_or_create(defaults={'title':j.text[4:]},memo=k.text,ldate=up_time,luser=up_user,iobool=True)
+    
+      
+
+print(memo_items)
+print(f'總共:{len(items)}筆資料')
+
